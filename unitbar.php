75e3968ba8793635a1875a5c51de0c8e9c233666
projectpixel16 <jasonflor.cenpri@gmail.com>
2019-04-17 16:19:44 +0800

    tefs

---
+++ unitbar.php
@@ -0,0 +1,5 @@
+<a href="viewrecord.php" class="btn btn-primary btn-sm">UNIT 1</a>
+<a href="viewrecord_u2.php" class="btn btn-primary btn-sm ">UNIT 2</a>
+<a href="" class="btn btn-primary btn-sm">UNIT 3</a>
+<a href="" class="btn btn-primary btn-sm">UNIT 4</a>
+<a href="" class="btn btn-primary btn-sm">UNIT 5</a>
